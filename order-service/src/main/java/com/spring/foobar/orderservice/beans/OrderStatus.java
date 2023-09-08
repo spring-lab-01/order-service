8e1ac13eb8b6845afa32b9264b92c6619c24e765
kumarhemant10 <hkchauhan022@gmail.com>
2023-09-07 20:30:02 -0400

    directory restructured

--- order-service/src/main/java/com/spring/foobar/orderservice/beans/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.spring.foobar.orderservice.beans;
-
-public enum OrderStatus {
-    CREATED, SHIPPED, DELIVERED, CANCELLED;
-}
--- order-service/src/main/resources/read_db_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE order_read.order
-(
-    id          INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id int8  NOT NULL,
-    order_date  timestamp,
-    product     VARCHAR(255),
-    status      VARCHAR(10)
-);
